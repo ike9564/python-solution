b894cccef2ac488bd211e0b5a7f16ac11c8a20e9
Ike <jake2011peralta@gmail.com>
2024-09-07 11:18:43 +0100

    Assignment changes

--- Python/playground.py
+++ Python/playground.py
@@ -132,9 +132,22 @@ def hundred_to_zero():
 print(count)
 
 # 2. Write a function that takes a list as an argument and returns a greeting personalized with each name on the list
+def greet_names(names):
+    greetings = []
+    for name in names:
+        greeting = f"Hello, {name}!"
+        greetings.append(greeting)
+    return greetings
 
 
 # 3. Write a function that takes 2 parameters: a first name and a last name and returns the full name in title case
+def full_name_in_title_case(first_name, last_name):
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+first_name = "Ike"
+last_name = "Ikeotuonye"
+print(full_name_in_title_case(first_name, last_name))
 
 
 # 4.write a function thats calculates degrees to farenheit
@@ -146,3 +159,15 @@ def degrees_to_fahrenheit(degrees):
 print(degrees_to_fahrenheit(45))
 
 
+"""
+The first thing you should do when you install Git is to set your user name and email address. This is important because every Git commit uses this information, and it’s immutably baked into the commits you start creating:
+
+git config --global user.name "John Doe"
+git config --global user.email johndoe@example.com
+
+
+Again, you need to do this only once if you pass the --global option, because then Git will always use that information for anything you do on that system. If you want to override this with a different name or email address for specific projects, you can run the command without the --global option when you’re in that project.
+
+Many of the GUI tools will help you do this when you first run them.
+
+"""
